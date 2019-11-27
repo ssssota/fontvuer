42b48dad54cf5672acf4a16cbbc9d155ceaf50ed
ssssota <tommysouta@gmail.com>
2019-11-27 13:10:56 +0900

    add vuetify

--- vue.config.js
+++ vue.config.js
@@ -1,9 +1,19 @@
 module.exports = {
-  pluginOptions: {
-    electronBuilder: {
-      builderOptions: {
-        publish: ['github']
+  "pluginOptions": {
+    "electronBuilder": {
+      "builderOptions": {
+        // List native deps here if they don't work
+        externals: ['font-manager'],
+        // If you are using Yarn Workspaces, you may have multiple node_modules folders
+        // List them all here so that VCP Electron Builder can find them
+        nodeModulesPath: ['../../node_modules', './node_modules'],
+        "publish": [
+          "github"
+        ]
       }
     }
-  }
+  },
+  "transpileDependencies": [
+    "vuetify"
+  ]
 }
