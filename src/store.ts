7e1f2bf1e893fd2f43795c0e2124c1f252dd0ccb
ssssota <tommysouta@gmail.com>
2019-12-03 14:19:28 +0900

    add initial value

--- src/store.ts
+++ src/store.ts
@@ -1,5 +1,12 @@
 import { IFontFamily } from './type';
 
+const INIT_SIZE = 32
+const INIT_WEIGHT = 400
+const INIT_KERNING = 0
+const INIT_ITALIC = false
+const INIT_PREVIEW = 'fontvuer'
+const INIT_FAV_ONLY = false
+
 export interface IState {
   previewText: string
   size: number
@@ -14,12 +21,12 @@ export interface IState {
 export const store = {
   debug: process.env.NODE_ENV !== 'production',
   state: {
-    previewText: 'fontvuer',
-    size: 16,
-    italic: false,
-    weight: 400,
-    kerning: 0,
-    favoriteOnly: false,
+    previewText: INIT_PREVIEW,
+    size: INIT_SIZE,
+    italic: INIT_ITALIC,
+    weight: INIT_WEIGHT,
+    kerning: INIT_KERNING,
+    favoriteOnly: INIT_FAV_ONLY,
     favFonts: [],
     detailFont: {} as IFontFamily
   } as IState,
@@ -32,19 +39,24 @@ export const store = {
     if (this.debug) console.log('Preview text →', _previewText)
     this.state.previewText = _previewText
   },
+  setSize(_size: number) {
+    if (typeof _size !== "number" || _size < 0 || 1000 < _size) _size = INIT_SIZE
+    if (this.debug) console.log('Size →', _size)
+    this.state.size = _size
+  },
   setItalic(_italic: boolean) {
     _italic = !!_italic
     if (this.debug) console.log('Italic →', _italic)
     this.state.italic = _italic
   },
   setWeight(_weight: number) {
-    if (typeof _weight !== "number" || _weight < 0 || 1000 < _weight) _weight = 500
+    if (typeof _weight !== "number" || _weight < 0 || 1000 < _weight) _weight = INIT_WEIGHT
     if (this.debug) console.log('Weight →', _weight)
     this.state.weight = _weight
   },
   setKerning(_kerning: number) {
-    if (typeof _kerning !== "number") _kerning = 0
-    if (this.debug) console.log('Weight →', _kerning)
+    if (typeof _kerning !== "number") _kerning = INIT_KERNING
+    if (this.debug) console.log('Kerning →', _kerning)
     this.state.kerning = _kerning
   },
   setFavoriteOnly(_favoriteOnly: boolean) {
