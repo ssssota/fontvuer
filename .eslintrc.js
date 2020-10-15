5323306e1c6755a344751b265994f15621996dda
Sotaro Tommykawa <tommysouta@gmail.com>
2020-10-15 23:44:11 +0900

    Fix some rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,21 +1,21 @@
 module.exports = {
   root: true,
   env: {
-    node: true
+    node: true,
   },
-  'extends': [
+  extends: [
     'plugin:vue/essential',
-    'eslint:recommended',
-    '@vue/typescript'
+    '@vue/airbnb',
+    '@vue/typescript/recommended',
   ],
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'semi': 'error',
-    'indent': ['error', 2],
-    'quotes': ['error', 'single']
+    semi: 'error',
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
   },
   parserOptions: {
-    parser: '@typescript-eslint/parser'
-  }
+    parser: '@typescript-eslint/parser',
+  },
 };
