d6cc1f8f871d477dbea4c5b81ab99e4641319a7c
Sotaro Tommykawa <tommysouta@gmail.com>
2020-10-25 20:18:24 +0900

    Fix eol check for win

--- .eslintrc.js
+++ .eslintrc.js
@@ -11,6 +11,7 @@ module.exports = {
   rules: {
     'no-console': 0,
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+    'linebreak-style': process.platform === 'win32' ? 'off' : ['error', 'unix'],
     semi: 'error',
     indent: ['error', 2],
     quotes: ['error', 'single'],
