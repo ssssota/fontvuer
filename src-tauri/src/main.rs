ca53e92b2f5ba6948f0bbba9ada9a9a2b9836eaf
ssssota <tommysouta@gmail.com>
2022-02-15 00:27:15 +0900

    add name decoder

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,18 +3,18 @@
     windows_subsystem = "windows"
 )]
 
-use font_kit::{handle::Handle, font::Font};
+use font_kit::handle::Handle;
 use font_kit::source::SystemSource;
 use serde::Serialize;
 use source::CustomDirSource;
 use std::collections::HashSet;
 use std::fs;
-use std::sync::Arc;
-use ttf_parser::{Face, Style, Width, name_id};
+use ttf_name_decoder::decode;
+use ttf_parser::{name_id, Face, PlatformId, Style, Width};
 
 mod source;
 
-#[derive(Serialize, Hash, PartialEq, Eq)]
+#[derive(Serialize, Hash, PartialEq, Eq, Debug)]
 struct FontDescriptor {
     installed: bool,
     path: String,
@@ -42,9 +42,11 @@ impl FontDescriptor {
 fn get_fonts(custom_dirs: Vec<String>, system_fonts: bool) -> Result<FontsResponse, String> {
     let mut fonts = HashSet::new();
     if system_fonts {
+        println!("load system fonts");
         if let Ok(handles) = SystemSource::new().all_fonts() {
             for h in handles {
                 if let Some(d) = FontDescriptor::from_handle(&h, true) {
+                    println!("handle");
                     fonts.insert(d);
                 }
             }
@@ -59,6 +61,7 @@ fn get_fonts(custom_dirs: Vec<String>, system_fonts: bool) -> Result<FontsRespon
             }
         }
     }
+    println!("{:?}", fonts);
     Ok(FontsResponse {
         fonts: fonts.into_iter().collect(),
     })
@@ -83,14 +86,45 @@ struct SystemFont {
 fn get_font_from_system(path: String, font_index: u32) -> Result<SystemFont, String> {
     let data = fs::read(&path).map_err(|e| e.to_string())?;
     let face = Face::from_slice(&data, font_index).map_err(|e| e.to_string())?;
-    let font = Font::from_bytes(Arc::new(data), font_index).map_err(|e| e.to_string())?;
     let names = face.names();
     Ok(SystemFont {
         path,
         index: font_index,
-        family: font.family_name(),
-        postscript: font.postscript_name(),
-        full: font.full_name(),
+        family: names
+            .get(name_id::FAMILY)
+            .map(|n| {
+                decode(
+                    n.name,
+                    platform_id_to_num(n.platform_id),
+                    n.encoding_id,
+                    n.language_id,
+                )
+            })
+            .flatten()
+            .ok_or("err")?,
+        postscript: names
+            .get(name_id::POST_SCRIPT_NAME)
+            .map(|n| {
+                decode(
+                    n.name,
+                    platform_id_to_num(n.platform_id),
+                    n.encoding_id,
+                    n.language_id,
+                )
+            })
+            .flatten(),
+        full: names
+            .get(name_id::FULL_NAME)
+            .map(|n| {
+                decode(
+                    n.name,
+                    platform_id_to_num(n.platform_id),
+                    n.encoding_id,
+                    n.language_id,
+                )
+            })
+            .flatten()
+            .ok_or("err")?,
         style: match face.style() {
             Style::Normal => "normal".to_string(),
             Style::Italic => "italic".to_string(),
@@ -128,19 +162,60 @@ struct CustomDirFont {
     weight: u16,
 }
 
+fn platform_id_to_num(platform_id: PlatformId) -> u16 {
+    match platform_id {
+        PlatformId::Unicode => 0,
+        PlatformId::Macintosh => 1,
+        PlatformId::Iso => 2,
+        PlatformId::Windows => 3,
+        PlatformId::Custom => 4,
+    }
+}
+
 #[tauri::command]
 fn get_font_with_data(path: String, font_index: u32) -> Result<CustomDirFont, String> {
     let data = fs::read(&path).map_err(|e| e.to_string())?;
     let face = Face::from_slice(&data, font_index).map_err(|e| e.to_string())?;
     let names = face.names();
-    let font = Font::from_bytes(Arc::new(data), font_index).map_err(|e| e.to_string())?;
     Ok(CustomDirFont {
         data: base64::encode(&data),
         path,
         index: font_index,
-        family: font.family_name(),
-        postscript: font.postscript_name(),
-        full: font.full_name(),
+        family: names
+            .get(name_id::FAMILY)
+            .map(|n| {
+                decode(
+                    n.name,
+                    platform_id_to_num(n.platform_id),
+                    n.encoding_id,
+                    n.language_id,
+                )
+            })
+            .flatten()
+            .ok_or("err")?,
+        postscript: names
+            .get(name_id::POST_SCRIPT_NAME)
+            .map(|n| {
+                decode(
+                    n.name,
+                    platform_id_to_num(n.platform_id),
+                    n.encoding_id,
+                    n.language_id,
+                )
+            })
+            .flatten(),
+        full: names
+            .get(name_id::FULL_NAME)
+            .map(|n| {
+                decode(
+                    n.name,
+                    platform_id_to_num(n.platform_id),
+                    n.encoding_id,
+                    n.language_id,
+                )
+            })
+            .flatten()
+            .ok_or("err")?,
         style: match face.style() {
             Style::Normal => "normal".to_string(),
             Style::Italic => "italic".to_string(),
@@ -163,7 +238,11 @@ fn get_font_with_data(path: String, font_index: u32) -> Result<CustomDirFont, St
 
 fn main() {
     tauri::Builder::default()
-        .invoke_handler(tauri::generate_handler![get_fonts, get_font_from_system, get_font_with_data])
+        .invoke_handler(tauri::generate_handler![
+            get_fonts,
+            get_font_from_system,
+            get_font_with_data
+        ])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
